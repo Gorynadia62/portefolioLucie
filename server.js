a5f3d20578733908a204a1c1e77a45a12cc89201
Iryste <jean-baptiste.hamel@hotmail.fr>
2025-04-14 10:06:34 +0200

    Initial commit

--- server.js
+++ server.js
@@ -1,25 +1,21 @@
-app.get("/Galerie", (req, res) => {
-  res.render("page/ Galerie");
-});
 app.get("/A propos", (req, res) => {
   res.render("page/ A propos");
 });
 
-app.get("/contacter", (req, res) => {
-  res.render("pages/contacter");
-});
-
-app.get("/", async (req, res) => {
+app.get("/Galerie", async (req, res) => {
   try {
     const imagesData = await images;
+    console.log(imagesData);
     res.render("pages/masonry", { images: imagesData });
   } catch (err) {
     console.log(err);
     res.render("pages/masonry", { images: [] });
   }
 });
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+});
+app.get("/contact", (req, res) => {
+  res.render("pages/contact");
 });
